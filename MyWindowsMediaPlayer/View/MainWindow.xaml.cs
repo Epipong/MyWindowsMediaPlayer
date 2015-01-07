89b5fbcf3dfecf0f230899defadff4c90ef3d51c
Davy <davytran1993@gmail.com>
2015-01-07 12:39:38 +0100

    add View folder + MainWindows.xaml to will be completed

--- MyWindowsMediaPlayer/View/MainWindow.xaml.cs
+++ MyWindowsMediaPlayer/View/MainWindow.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MyWindowsMediaPlayer
+namespace MyWindowsMediaPlayer.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
         }
