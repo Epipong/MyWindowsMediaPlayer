cf8d708d73e7e96a1f4fd4509c84d8a63a26e223
Davy <davytran1993@gmail.com>
2015-01-10 00:54:13 +0100

    add source images and add media bars: audio, video and image

---
+++ MyWindowsMediaPlayer/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication3
+{
+    public partial class MainWindow : Window
+    {
+        void Play_Click(object sender, RoutedEventArgs e)
+        {
+                myMedia.Play();
+        }
+
+        void Pause_Click(object sender, RoutedEventArgs e)
+        {
+                myMedia.Pause();
+        }
+
+        void Stop_Click(object sender, RoutedEventArgs e)
+        {
+                myMedia.Stop();
+        }
+
+        void Photo_Click(object sender, RoutedEventArgs e)
+        {
+                myPhoto.Play();
+        }
+
+/*        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                myMedia.Play();
+            }
+            catch {}
+            using (var soundPlayer = new System.Media.SoundPlayer(@"c:\Windows\Media\Dangerous.wav"))
+            {
+                soundPlayer.Play();
+            }
+
+        }
+    */
+    }
+}
---
+++ MyWindowsMediaPlayer/View/AudioBar.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyWindowsMediaPlayer.View
+{
+    /// <summary>
+    /// Interaction logic for AudioBar.xaml
+    /// </summary>
+    public partial class AudioBar : UserControl
+    {
+        public AudioBar()
+        {
+            InitializeComponent();
+        }
+
+        private void OnPlayClick(object sender, RoutedEventArgs e)
+        {
+            Play.Visibility = Visibility.Collapsed;
+            Pause.Visibility = Visibility.Visible;
+        }
+
+        private void OnPauseClick(object sender, RoutedEventArgs e)
+        {
+            Pause.Visibility = Visibility.Collapsed;
+            Play.Visibility = Visibility.Visible;
+        }
+    }
+}
